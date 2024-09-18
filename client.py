97e2f17b451e34e69afcae258769c21e8c206d3a
B. Pan <bp@Bs-Mac-mini.local>
2024-09-18 12:57:56 -0700

    Added main function and constant variable

--- client.py
+++ client.py
@@ -1,30 +1,38 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 5999))
+HOST = 'localhost'
+PORT = 5999
+QUIT = 'quit'
 
-done = False
 
-while not done:
-    client_msg = input("Message: ")
-    
-    # If the client types "quit", exit the loop
-    if client_msg == 'quit':
-        done = True
-        client.send("quit".encode('utf-8'))  # Send "quit" to the server
-        print("Exiting chat...")
-        break  # Exit the loop after sending quit message
-    
-    client.send(client_msg.encode('utf-8'))
-    
-    # Receive the server's message
-    server_msg = client.recv(1024).decode('utf-8')
-    # If the server sends "quit", exit the loop
-    if server_msg == 'quit':
-        done = True
-        print("Server closed the connection. Exiting chat...")
-    else:
-        print(server_msg)
+def main(): 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
 
-print("Connection closed")
-client.close()
+    done = False
+    while not done:
+        client_msg = input("Message: ")
+        
+        # If the client types "quit", exit the loop
+        if client_msg == QUIT:
+            done = True
+            client.send(QUIT.encode('utf-8'))  # Send "quit" to the server
+            print("Exiting chat...")
+            break  # Exit the loop after sending quit message
+        
+        client.send(client_msg.encode('utf-8'))
+        
+        # Receive the server's message
+        server_msg = client.recv(1024).decode('utf-8')
+        # If the server sends "quit", exit the loop
+        if server_msg == QUIT:
+            done = True
+            print("Server closed the connection. Exiting chat...")
+        else:
+            print(server_msg)
+
+    print("Connection closed")
+    client.close()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,30 +1,38 @@
 import socket 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 5999))
-server.listen(1)  # Listening for 1 connection
+HOST = 'localhost'
+PORT = 5999
+QUIT = 'quit'
 
-client, addr = server.accept()
 
-done = False
-while not done:
-    # Receive the client's message
-    msg = client.recv(1024).decode('utf-8')
-    
-    if msg == 'quit':
-        print("Client left")
-        done = True  # Exit the loop after client sends 'quit'
-    else:
-        print(msg)
+def main(): 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(1)  # Listening for 1 connection
+    client, addr = server.accept()
+
+    done = False
+    while not done:
+        # Receive the client's message
+        msg = client.recv(1024).decode('utf-8')
         
-    # If the client didn't quit, allow the server to send a message
-    if not done:
-        server_message = input("Message: ")
-        
-        if server_message == 'quit':
-            done = True  # Exit the loop after server sends 'quit'
-        client.send(server_message.encode('utf-8'))
-        
-print("Connection closed")
-client.close()  # Close the connection after the loop
-server.close()  # Close the server
+        if msg == QUIT:
+            print("Client left")
+            done = True  # Exit the loop after client sends 'quit'
+        else:
+            print(msg)
+            
+        # If the client didn't quit, allow the server to send a message
+        if not done:
+            server_message = input("Message: ")
+            
+            if server_message == QUIT:
+                done = True  # Exit the loop after server sends 'quit'
+            client.send(server_message.encode('utf-8'))
+            
+    print("Connection closed")
+    client.close()  # Close the connection after the loop
+    server.close()  # Close the server
+
+if __name__ == '__main__':
+    main()
