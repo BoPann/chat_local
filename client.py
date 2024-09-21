dcc60257fac94f7f42b5ea434e7f46a0f2ed7418
B. Pan <bp@Bs-Mac-mini.local>
2024-09-20 20:47:40 -0700

    Added multi thredding to handle multiple clients

--- client.py
+++ client.py
@@ -1,18 +1,50 @@
 import socket
+import threading
 
 HOST = 'localhost'
-PORT = 5999
+PORT = 6999
 QUIT = 'quit'
 
+def listen_message_from_server(client):
+
+    while True:
+        msg = client.recv(2048).decode('utf-8')
+        if msg != '':
+            username, msg = msg.split('~')
+            print(f"[{username}] {msg}")
+
+def send_message_to_server(client):
+    while True:
+        msg = input("Message: ")
+        if msg != '':
+            client.sendall(msg.encode())
+        
+
+def communicate_to_server(client):
+
+    username = input("Input your username: ")
+    if username != '':
+        # send the message to server with username
+        client.sendall(username.encode())
+    else:
+        print("Username can't be empty!")
+        exit(0)
+
+    threading.Thread(target=listen_message_from_server, args=(client, )).start()
+    send_message_to_server(client)
 
 def main(): 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
 
-    done = False
-    while not done:
-        client_msg = input("Message: ")
+    # assign port to socekt
+    try:
+        client.connect((HOST, PORT))
+    except:
+        print(f"Unable to bind to host {HOST} and port {PORT}")
+
+    communicate_to_server(client)
         
+    """
         # If the client types "quit", exit the loop
         if client_msg == QUIT:
             done = True
@@ -30,7 +62,7 @@ def main():
             print("Server closed the connection. Exiting chat...")
         else:
             print(server_msg)
-
+    """
     print("Connection closed")
     client.close()
 
--- server.py
+++ server.py
@@ -1,38 +1,90 @@
-import socket 
+# Import required modules
+import socket
+import threading
 
 HOST = 'localhost'
-PORT = 5999
+PORT = 6999
+LISTENER_LIMIT = 5
+active_clients = [] # List of all currently connected users
 QUIT = 'quit'
 
+"""
+listen for upcoming messages from a client
+"""
+def listen_for_messages(client, username):
 
-def main(): 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(1)  # Listening for 1 connection
-    client, addr = server.accept()
+    while True:
 
-    done = False
-    while not done:
-        # Receive the client's message
-        msg = client.recv(1024).decode('utf-8')
-        
-        if msg == QUIT:
-            print("Client left")
-            done = True  # Exit the loop after client sends 'quit'
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+
         else:
-            print(msg)
-            
-        # If the client didn't quit, allow the server to send a message
-        if not done:
-            server_message = input("Message: ")
-            
-            if server_message == QUIT:
-                done = True  # Exit the loop after server sends 'quit'
-            client.send(server_message.encode('utf-8'))
-            
-    print("Connection closed")
-    client.close()  # Close the connection after the loop
-    server.close()  # Close the server
+            print(f"The message send from client {username} is empty")
+
+
+"""
+send message to a single client
+"""
+def send_message_to_client(client, message):
+
+    client.sendall(message.encode())
+
+"""
+send message to all clients
+"""
+def send_messages_to_all(message):
+    
+    for user in active_clients:
+
+        send_message_to_client(user[1], message)
+
+
+"""handle client """
+def client_handler(client):
+    
+
+    while True:
+
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            prompt_message = "SERVER~" + f"{username} added to the chat"
+            send_messages_to_all(prompt_message)
+            break
+        else:
+            print("Client username is empty")
+
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
+
+"""
+Main function
+"""
+def main():
+
+    # create a socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # bind socket to a port
+    try:
+        server.bind((HOST, PORT))
+        print(f"Running the server on {HOST} {PORT}")
+    except:
+        print(f"Unable to bind to host {HOST} and port {PORT}")
+
+    # Set server limit
+    server.listen(LISTENER_LIMIT)
+
+    # This while loop will keep listening to client connections
+    while True:
+
+        client, address = server.accept()
+        print(f"Successfully connected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
+
 
 if __name__ == '__main__':
     main()
